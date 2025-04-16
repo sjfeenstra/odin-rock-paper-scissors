56910a2f6e8a375ceb7f608c09c6a8654931f76f
sjfeenstra <sjoerdfeenstra_7@hotmail.com>
2025-04-16 21:13:11 +0200

    create html and js file with pseudo code

---
+++ javascript.js
@@ -0,0 +1,9 @@
+console.log("Hello world")
+
+// get computer choice
+// get random item from list [Rock, paper, scissors]
+// return random item
+
+// request user input on rock paper or scissors
+// check if input is correct
+// if not request new answer
